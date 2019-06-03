6aae07e7b720a3499a38774f901e140150b8aa13
ItsRicmor <ricardojms1999@gmail.com>
2019-06-02 18:02:49 -0600

    Clean proyect

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+/*
+Integrantes:
+    Ricardo Morataya Sandoval
+    Luis Rodriguez Baltodano
+    Arlen Vargas Gálvez
+    Eddie Alfaro Villegas
+    Erick Parra Centeno
+*/
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
